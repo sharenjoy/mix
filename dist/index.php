6065099cdc7135623e141530786837c0c3f11520
Ronald <ronald.jian@gmail.com>
2017-10-19 13:43:27 +0800

    add php mix function

---
+++ dist/index.php
@@ -0,0 +1,27 @@
+<?php
+  require_once '../vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="zh-TW">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="images/favicon.ico">
+
+    <title></title>
+    
+    <!-- styles -->
+    <link href="<?php echo mix('css/app.css'); ?>" rel="stylesheet">
+  </head>
+
+  <body>
+    
+    <!-- script -->
+    <script src="<?php echo mix('js/app.js'); ?>"></script>
+
+  </body>
+</html>
